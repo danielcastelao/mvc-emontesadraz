4fcb468a73a97063fc64c01c194362789a8c0900
Esteban <emontesadraz@danielcastelao.org>
2024-05-03 10:29:03 +0200

    Coches creados e instanciados en la main

--- src/cod/mvc/Controller.java
+++ src/cod/mvc/Controller.java
@@ -2,6 +2,22 @@ package cod.mvc;
 
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Model.crearCoche("1234ABC", "Seat Ibiza", 100);
+        Model.crearCoche("5678DEF", "Ford Focus", 120);
+        Model.crearCoche("9012GHI", "Renault Clio", 80);
+
+        Model.cambiarVelocidad("1234ABC", 110);
+        Model.cambiarVelocidad("5678DEF", 130);
+        Model.cambiarVelocidad("9012GHI", 90);
+
+        Model.getVelocidad("1234ABC");
+        Model.getVelocidad("5678DEF");
+        Model.getVelocidad("9012GHI");
+
+        Model.getCoche("1234ABC").acelerar();
+        Model.getCoche("5678DEF").acelerar();
+        Model.getCoche("9012GHI").acelerar();
+
+        View view = new View();
     }
 }
