33545135de22248536c995cbccf86dddad61b474
Esteban <emontesadraz@danielcastelao.org>
2024-05-24 09:19:13 +0200

    crearCoche desde controller y main en App

--- src/com/cod/mvc/App.java
+++ src/com/cod/mvc/App.java
@@ -2,10 +2,12 @@ package com.cod.mvc;
 
 import com.cod.mvc.Model.Model;
 import com.cod.mvc.View.View;
+import com.cod.mvc.controller.Controller;
 
 public class App {
     public static void main(String[] args) {
-        Model.crearCoche("1234ABC", "Seat", 120);
+        Controller controller = new Controller();
+        controller.crearCoche("1234ABC", "Seat", 50);
         View.muestraVelocidad("1234ABC", Model.getVelocidad("1234ABC"));
         Model.cambiarVelocidad("1234ABC", 60);
         View.muestraVelocidad("1234ABC", Model.getVelocidad("1234ABC"));
--- src/com/cod/mvc/controller/Controller.java
+++ src/com/cod/mvc/controller/Controller.java
@@ -4,23 +4,8 @@ import com.cod.mvc.Model.Model;
 import com.cod.mvc.View.View;
 
 public class Controller {
-    public static void main(String[] args) {
-        Model.crearCoche("1234ABC", "Seat Ibiza", 100);
-        Model.crearCoche("5678DEF", "Ford Focus", 120);
-        Model.crearCoche("9012GHI", "Renault Clio", 80);
-
-        Model.cambiarVelocidad("1234ABC", 110);
-        Model.cambiarVelocidad("5678DEF", 130);
-        Model.cambiarVelocidad("9012GHI", 90);
-
-        Model.getVelocidad("1234ABC");
-        Model.getVelocidad("5678DEF");
-        Model.getVelocidad("9012GHI");
-
-        Model.getCoche("1234ABC").acelerar();
-        Model.getCoche("5678DEF").acelerar();
-        Model.getCoche("9012GHI").acelerar();
-
+        public void crearCoche(String matricula, String modelo, int velocidad){
+            Model.crearCoche(matricula, modelo, velocidad);
+        }
         View view = new View();
     }
-}
